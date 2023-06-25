24303e9cc7128d93251ee6d9d75fd59c4b329fb8
AnggitaRisqiNC <anggitarisqi0907@gmail.com>
2023-06-25 10:52:36 +0700

    Commit

---
+++ Praktikum7.sql
@@ -0,0 +1,98 @@
+CREATE DATABASE Praktikum7;
+USE Praktikum7;
+DROP DATABASE Praktikum7;
+
+CREATE TABLE Perusahaan(
+id_p VARCHAR(10) PRIMARY KEY,
+nama VARCHAR(45) NOT NULL,
+alamat VARCHAR(45) DEFAULT NULL
+);
+
+CREATE TABLE Departemen(
+id_dept VARCHAR(10) PRIMARY KEY,
+nama VARCHAR(45) NOT NULL,
+id_p VARCHAR(10) NOT NULL,
+manajer_nik VARCHAR(10) DEFAULT NULL
+);
+
+CREATE TABLE Karyawan(
+nik VARCHAR(10) PRIMARY KEY,
+nama VARCHAR(45) NOT NULL,
+id_dept VARCHAR(10) NOT NULL,
+sup_nik VARCHAR(10) DEFAULT NULL,
+gaji_pokok INT
+);
+
+CREATE TABLE Project(
+id_proj VARCHAR(10) PRIMARY KEY,
+nama VARCHAR(45) NOT NULL,
+tgl_mulai DATETIME,
+tgl_selesai DATETIME,
+status TINYINT(1)
+);
+
+CREATE TABLE Project_detail(
+id_proj VARCHAR(10) NOT NULL,
+nik VARCHAR(10) NOT NULL
+);
+
+INSERT INTO Perusahaan (id_p, nama, alamat) VALUES
+('P01', 'Kantor Pusat', NULL),
+('P02', 'Cabang Bekasi', NULL);
+SELECT * FROM Perusahaan;
+
+INSERT INTO Departemen (id_dept, nama, id_p, manajer_nik) VALUES
+('D01', 'Produksi', 'P02', 'N01'),
+('D02', 'Marketing', 'P01', 'N03'),
+('D03', 'RnD', 'P02', NULL),
+('D04', 'Logistik', 'P02', NULL);
+SELECT * FROM Departemen;
+
+INSERT INTO Karyawan (nik, nama, id_dept, sup_nik, gaji_pokok) VALUES
+('N01', 'Ari', 'D01', NULL, 2000000),
+('N02', 'Dina', 'D01', NULL, 2500000),
+('N03', 'Rika', 'D03', NULL, 2400000),
+('N04', 'Ratih', 'D01', 'N01', 3000000),
+('N05', 'Riko', 'D01', 'N01', 2800000),
+('N06', 'Dani', 'D02', NULL, 2100000),
+('N07', 'Anis', 'D02', 'N06', 5000000),
+('N08', 'Dika', 'D02', 'N06', 4000000),
+('N09', 'Raka', 'D03', 'N06', 2000000);
+SELECT * FROM Karyawan;
+
+INSERT INTO Project (id_proj, nama, tgl_mulai, tgl_selesai, status) VALUES
+('PJ01', 'A', '2019-01-10', '2019-03-10', '1'),
+('PJ02', 'B', '2019-02-15', '2019-04-10', '1'),
+('PJ03', 'C', '2019-03-21', '2019-05-10', '1');
+SELECT * FROM Project;
+
+INSERT INTO Project_detail (id_proj, nik) VALUES
+('PJ01', 'N01'),
+('PJ01', 'N02'),
+('PJ01', 'N03'),
+('PJ01', 'N04'),
+('PJ01', 'N05'),
+('PJ01', 'N07'),
+('PJ01', 'N08'),
+('PJ02', 'N01'),
+('PJ02', 'N03'),
+('PJ02', 'N05'),
+('PJ03', 'N03'),
+('PJ03', 'N07'),
+('PJ03', 'N08');
+SELECT * FROM Project_detail;
+
+ALTER TABLE Project_detail
+ADD CONSTRAINT fk_project_detail_id_proj FOREIGN KEY (id_proj) REFERENCES Project (id_proj);
+
+SELECT nik, nama, gaji_pokok FROM Karyawan WHERE gaji_pokok > (SELECT MAX(gaji_pokok) FROM Karyawan WHERE nama = 'Riko');
+SELECT nik, nama FROM Karyawan WHERE sup_nik = (SELECT sup_nik FROM Karyawan WHERE nama = 'Riko');
+SELECT nik, nama FROM Karyawan WHERE sup_nik = (SELECT nik FROM Karyawan WHERE nama = 'Ari');
+SELECT nik, nama, gaji_pokok FROM Karyawan WHERE gaji_pokok = (SELECT MIN(gaji_pokok) FROM Karyawan);
+SELECT nik, nama, gaji_pokok FROM Karyawan WHERE gaji_pokok IN (SELECT MIN(gaji_pokok) FROM Karyawan GROUP BY id_dept);
+
+SELECT nik, nama, id_dept FROM Karyawan WHERE id_dept = (SELECT id_dept FROM Karyawan WHERE nama = 'Dika');
+SELECT nik, nama, id_dept, gaji_pokok FROM karyawan WHERE gaji_pokok > (SELECT AVG(gaji_pokok) FROM Karyawan) ORDER BY gaji_pokok DESC;
+SELECT nik, nama FROM Karyawan WHERE id_dept IN (SELECT id_dept FROM Karyawan WHERE nama LIKE '%K%');
+SELECT karyawan.nik, karyawan.nama, karyawan.id_dept FROM karyawan JOIN departemen ON karyawan.id_dept = departemen.id_dept WHERE departemen.id_p = 'P01';
+SELECT DISTINCT k1.nik, k1.nama FROM karyawan k1 JOIN karyawan k2 ON k1.id_dept = k2.id_dept WHERE k1.gaji_pokok > (SELECT AVG(gaji_pokok) FROM karyawan WHERE nama LIKE '%K%');
